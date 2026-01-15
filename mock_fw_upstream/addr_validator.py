72194d2d9bbb70a2581c4fd826507983f621a639
Zinc Lim <zinclim@meta.com>
2026-01-15 15:47:15 -0800

    Add copyright headers to address open source requirement
    Reviewed By: timot3
    
    Differential Revision: D90808372
    
    fbshipit-source-id: 64bcb8fd84c31fcac2723d65d9703283b69a26e9

--- mock_fw_upstream/addr_validator.py
+++ mock_fw_upstream/addr_validator.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the Apache 2.0 license found in the
+# LICENSE file in the root directory of this source tree.
+
 from mock_fw_upstream.utils import bytes_to_int
 
 # Constants from iatu_mapping[] in durga.c
--- mock_fw_upstream/tlv.py
+++ mock_fw_upstream/tlv.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the Apache 2.0 license found in the
+# LICENSE file in the root directory of this source tree.
+
 import errno
 import logging
 from dataclasses import dataclass
